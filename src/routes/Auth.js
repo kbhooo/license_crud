b643c7ff6ad97ef8f7f6b3b43d989dbbfa303b58
kbhooo <ironman035614@GMAIL.COM>
2021-05-16 12:11:04 +0900

    #2 Auth - Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,42 @@
 import React from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (e) => {
+      const {target: {name, value}} = e;
+      if(name === "email"){
+        setEmail(value)
+      } else if (name === "password") {
+        setPassword(value);
+      }
+    };
+    const onSubmit = (e) => {
+      e.preventDefault();
+    };
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input 
+            name="email" 
+            type="text" 
+            placeholder="Email" 
+            required value={email}
+            onCange={onCange}
+          />
+          <input 
+            name="password"
+            type="password" 
+            placeholder="Password" 
+            required
+            value="password"
+            onCange={onCange} 
+          />
+          <input type="submit" value="Login" />
+        </form>
+        <button>Continue with Google</button>
+      </div>
+    );
+}
+
 export default Auth;
