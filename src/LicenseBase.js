797af3f9765e172941cfd74f2d24c980cd145ab1
kbhooo <ironman035614@GMAIL.COM>
2021-05-16 21:41:02 +0900

    #3 License - Create License

--- src/LicenseBase.js
+++ src/LicenseBase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/firebase-database";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +16,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authServise = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
+import { dbService } from "licenseBase";
 
 const Home = () => {
-  const [nweet, setNweet] = useState("");
-  const onSubmit = (e) => {
+  const [name, setName] = useState("");
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await dbService.collection("TestLicense").add({
+      name,
+      dateIssued: Date.now(),
+    });
+    setName("");
   }
   const onChange = (e) => {
     const { 
       target: { value },
     } = e;
-    setNweet(value);
+    setName(value);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind?" maxLength={120} />
-        <input type="submit" value="라이센스" />
+        <input onChange={onChange} value={name} type="text" placeholder="라이센스 이름을 입력하세요." maxLength={120} />
+        <input type="submit" value="생성" />
       </form>
     </div>
   );
