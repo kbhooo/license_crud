649a6bf7a862bb59d4f3c2f11e3cc8f1563ca7f0
kbhooo <ironman035614@GMAIL.COM>
2021-05-17 13:29:33 +0900

    #3 License - Get License

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "licenseBase";
 
 const Home = () => {
   const [name, setName] = useState("");
+  const [names, setNames] = useState([]);
+  const getNames = async()=>{
+    const dbNames = await dbService.collection("TestLicense").get();
+    dbNames.forEach((document) => {
+      const nameObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNames((prev) => [nameObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNames();
+  }, [])
   const onSubmit = async (e) => {
     e.preventDefault();
     await dbService.collection("TestLicense").add({
@@ -17,12 +31,26 @@ const Home = () => {
     } = e;
     setName(value);
   };
+  console.log(names);
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={name} type="text" placeholder="라이센스 이름을 입력하세요." maxLength={120} />
+        <input
+          value={name}
+          onChange={onChange}
+          type="text"
+          placeholder="라이센스 이름을 입력하세요."
+          maxLength={120}
+        />
         <input type="submit" value="생성" />
       </form>
+      <div>
+        {names.map((name) => (
+          <div key={name.id}>
+            <h4>{name.name}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
